1a6b27e4ab00f3c5797c553d05fcb33eddc66f51
Sashank <sashank2408@gmail.com>
2023-02-10 18:37:49 +0530

    shinra tensei

---
+++ dashboard.py
@@ -0,0 +1,100 @@
+from server_api.db import MonDb
+import streamlit as st
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+from server_api.api.graph import App
+from configparser import ConfigParser
+import os
+import sys
+from collections import Counter
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+#from streamlit import cli as stcli
+
+
+def main():
+    if 'detected_symptoms' not in st.session_state:
+        st.session_state.detected_symptoms = 'value'
+
+    if 'diseases' not in st.session_state:
+        st.session_state.diseases = 'value'
+    print(st.session_state['detected_symptoms'])
+
+    col1 = st.container()
+    col2 = st.container()
+    col3 = st.container()
+
+    col1, col2, col3 = st.columns(3)
+    refresh()
+
+    common_symptoms()
+    common_pred_diseases()
+
+
+def refresh():
+
+    mondb = MDB.get_data()
+    query = "MATCH (s)-[r] -> (d) RETURN s, d"
+
+    detected_simp = []
+    disease = []
+    for i in mondb:
+        disease += [i[0] for i in i['diseases']]
+        detected_simp += i['detected_symptoms']
+
+    cq = GAp.custom_query(query)
+    print(cq)
+
+    st.session_state['detected_symptoms'] = detected_simp
+    st.session_state['diseases'] = disease
+    # return mondb, cq
+
+
+def common_symptoms():
+    counts = Counter(st.session_state['detected_symptoms']).most_common()
+    df_counts = pd.DataFrame(counts, columns=['symp', 'count'])
+    print(df_counts)
+    fig, x = plt.subplots()
+    x.pie(df_counts['count'], labels=df_counts['symp'], autopct='%1.1f%%')
+    st.pyplot(fig)
+
+
+def common_pred_diseases():
+    counts = Counter(st.session_state['diseases']).most_common()
+    df_counts = pd.DataFrame(counts, columns=['dis', 'count'])
+    print(df_counts)
+    fig, x = plt.subplots()
+    x.pie(df_counts['count'], labels=df_counts['dis'], autopct='%1.1f%%')
+    st.pyplot(fig)
+
+
+if __name__ == "__main__":
+
+    cfilename = os.path.join('server_api', 'config.ini')
+    config = ConfigParser()
+    config.read(cfilename)
+    DB = config['DB']
+    GRAPH = config['graph']
+
+    # initializing dbs
+    passwd = DB['password']
+    uri = DB['uri'].format(passwd)
+    db_name = DB['dbname']
+    collection = DB['collection']
+
+    try:
+        MDB = MonDb(uri, db_name, collection)
+    except:
+        uri = DB['uri2'].format(passwd)
+        MDB = MonDb(uri, db_name, collection)
+
+    GAp = App(GRAPH['uri'], GRAPH['username'], GRAPH['password'])
+    # if st._is_running_with_streamlit:
+    #     main()
+    # else:
+    #     sys.argv = ["streamlit", "run", sys.argv[0]]
+    #     sys.exit(stcli.main())
+
+    main()
--- server_api/db.py
+++ server_api/db.py
@@ -1,84 +1,79 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
-import json 
+import json
 
 
 class MonDb:
     def __init__(self, uri, db_name, collection_name):
-        self.db  = self.connect_db(uri, db_name)
-        self.col = collection_name 
-    
+        self.db = self.connect_db(uri, db_name)
+        self.col = collection_name
+
     def connect_db(self, uri, name):
         client = MongoClient(uri)
         return client[name]
-    
-    def user_in_db(self, id, return_type = 'obj'):
+
+    def get_data(self):
+        return self.db[self.col].find()
+
+    def user_in_db(self, id, return_type='obj'):
         history = self.db[self.col].find_one({
-            'userId' : id
-            })
-        
+            'userId': id
+        })
+
         if return_type == 'bool':
             return history != None
-        
+
         return history
-    
+
     def upload_history(self, userId, history, diseases, symptoms):
         data = {
-            'userId' : userId,
-            'history' : history,
-            'diseases' : diseases,
-            'detected_symptoms' : symptoms
+            'userId': userId,
+            'history': history,
+            'diseases': diseases,
+            'detected_symptoms': symptoms
         }
         self.db[self.col].insert_one(data)
-        
+
+
 class LocDb:
-    def __init__(self, filename = 'local_history.json'):
+    def __init__(self, filename='local_history.json'):
         self.filename = filename
         self.create_db()
-        
+
     def create_db(self):
-        data = {'userId' : ['symptoms']}
+        data = {'userId': ['symptoms']}
         with open(self.filename, 'w') as file:
-            json.dump(data, file, indent = 6)
-                
+            json.dump(data, file, indent=6)
+
     def load_db(self):
         data = None
         with open(self.filename, 'r') as file:
             data = json.load(file)
-        return data 
-    
+        return data
+
     def save_db(self, data):
         with open(self.filename, 'w') as file:
-            json.dump(data, file, indent = 6)
-    
+            json.dump(data, file, indent=6)
+
     def add_user_symp(self, userId, data):
         jdb = self.load_db()
         if userId in jdb.keys():
             jdb[userId] += data
         else:
             jdb[userId] = data
-            
+
         self.save_db(jdb)
-    
+
     def del_user(self, userId):
         jdb = self.load_db()
-        jdb = {k : v for k,v in jdb.items() if k != userId}  
+        jdb = {k: v for k, v in jdb.items() if k != userId}
         self.save_db(jdb)
-        
+
     def get_user_symp(self, userId):
         try:
             jdb = self.load_db()
             symps = jdb[userId]
         except KeyError as err:
             pass
-        return symps 
-        
-        
-        
-        
-    
-            
-            
-        
-        
+        return symps
